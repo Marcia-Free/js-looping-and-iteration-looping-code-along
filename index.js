8358eceafe669afbd4d940f9d051884ca59055e0
Marcia-Free <MarciaFreeman2011@gmail.com>
2020-12-11 19:48:59 -0600

    commit tree 1e54735eab9f3dd9348dc5223680cab6945aab2d

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+
+
+function writeCards(namesArray, event) {
+    let messageArray = []
+
+    for (let i = 0; i < namesArray.length; i++) {
+        messageArray.push(`Thank you, ${namesArray[i]}, for the wonderful surprise gift!`)
+    }
+    return messageArray
+}
+
+function countDown(num) {
+    let i = num
+    while (i >= 0) {
+        console.log(i)
+        i--
+    }
+}
